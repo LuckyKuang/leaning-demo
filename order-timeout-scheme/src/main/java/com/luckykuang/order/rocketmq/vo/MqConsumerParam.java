ef8e8d6eedbf82a85ad55c8a6cf6475d1b9c6e4c
luckykuang <luckykuang@foxmail.com>
2023-08-04 18:19:34 +0800

    feat: - 新增rocketmq demo

---
+++ order-timeout-scheme/src/main/java/com/luckykuang/order/rocketmq/vo/MqConsumerParam.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2015-2023 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.luckykuang.order.rocketmq.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyContext;
+import org.apache.rocketmq.client.consumer.listener.ConsumeOrderlyContext;
+import org.apache.rocketmq.common.message.MessageExt;
+
+import java.util.List;
+
+/**
+ * @author luckykuang
+ * @date 2023/8/4 17:08
+ */
+@Data
+@AllArgsConstructor
+public class MqConsumerParam {
+    /**
+     * 消息
+     */
+    private List<MessageExt> list;
+
+    /**
+     * 非顺序消费上下文
+     */
+    private ConsumeConcurrentlyContext consumeConcurrentlyContext;
+
+    /**
+     * 顺序消费上下文
+     */
+    private ConsumeOrderlyContext consumeOrderlyContext;
+}
